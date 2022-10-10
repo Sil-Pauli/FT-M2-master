89e4fab978d8823cb80c570076821396e1851e7a
Sil-paulli <sil.pauli.93@gmai.com>
2022-10-10 02:58:49 -0300

    homework terminada

--- FT-M2-master/07-React-Estilos/homework/src/components/Card.jsx
+++ FT-M2-master/07-React-Estilos/homework/src/components/Card.jsx
@@ -1,6 +1,22 @@
 import React from 'react';
+import s from './Card.module.css';
 
 export default function Card(props) {
   // acá va tu código
-  return <div>Card Component</div>
+  return (
+    <div className={s.cuadrado}>
+      <button className={s.btnClose} onClick={props.onClose}> X </button>
+      <h3 className={s.name}> {props.name}</h3>
+      <img src={ `http://openweathermap.org/img/wn/${props.img}@2x.png`} alt="Img not found" />
+        <div className={s.minmax}> 
+          <div>
+            <p>MIN        {props.min}</p>
+          </div>
+          <div>
+            <p>MAX  {props.max}</p>
+          </div>
+        </div> 
+      
+    </div>
+   )
 };
--- FT-M2-master/07-React-Estilos/homework/src/components/SearchBar.jsx
+++ FT-M2-master/07-React-Estilos/homework/src/components/SearchBar.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import s from './SearchBar.module.css';
 
-export default function SearchBar(props) {
+export default function SearchBar({onSearch}) {
   // acá va tu código
-  return <div>Search Bar Component</div>
+  return (
+  <div className={s.cuadrado}>
+    <input type='text'/>
+    <button className={s.btnClose} onClick={() => onSearch('Agregando ciudad..')}>Agregar</button>
+  </div>
+  )
 };
